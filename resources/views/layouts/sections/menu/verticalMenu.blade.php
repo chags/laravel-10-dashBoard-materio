4b7ca32c182879b0b094f9e7720b94d07d53e4da
chags <cristiano.chagas@th7.com.br>
2024-05-28 10:04:08 -0300

    logo up

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -4,9 +4,8 @@
   <div class="app-brand demo">
     <a href="{{url('/')}}" class="app-brand-link">
       <span class="app-brand-logo demo me-1">
-        @include('_partials.macros',["height"=>20])
+        <img src="{{ asset('assets/img/logo/logo.png') }}" alt="">
       </span>
-      <span class="app-brand-text demo menu-text fw-semibold ms-2">{{config('variables.templateName')}}</span>
     </a>
 
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
